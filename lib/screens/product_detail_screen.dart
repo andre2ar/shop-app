37897b0f47b1d79754f2b99c20089dc04d10fc15
André <andre2ar@outlook.com>
2020-05-07 16:33:22 -0300

    Product details

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,20 +3,53 @@ import 'package:provider/provider.dart';
 import 'package:shopapp/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  static String route = '/product-details';
+  static const route = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
-
+    final productId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(
       context,
       listen: false,
     ).findById(productId);
-
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
