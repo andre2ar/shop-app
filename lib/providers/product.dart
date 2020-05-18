8d15d3cceaefabba7c47aefd99c407de9472d0e0
André <andre2ar@outlook.com>
2020-05-18 15:10:42 -0300

    Orders

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,31 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouritesStatus() {
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavouritesStatus() async {
+    final oldStatus = isFavourite;
+
     isFavourite = !isFavourite;
     notifyListeners();
+
+    final url = 'https://flutterstore-42eae.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {'isFavourite': isFavourite},
+        ),
+      );
+
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
