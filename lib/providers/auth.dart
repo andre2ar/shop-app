66b206c7e17affd98841d6cadd3b04e868c5929d
André <andre2ar@outlook.com>
2020-05-20 09:54:47 -0300

    Auth screen

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shopapp/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -13,21 +14,29 @@ class Auth with ChangeNotifier {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBr2_xcq-qV9QTkoajScpJfYtuLd5Eot80';
 
-    return await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> login(String email, String password) async {
-    final response = _authenticate(email, password, 'signInWithPassword');
+    return _authenticate(email, password, 'signInWithPassword');
   }
 
   Future<void> signup(String email, String password) async {
-    final response = _authenticate(email, password, 'signUp');
+    return _authenticate(email, password, 'signUp');
   }
 }
