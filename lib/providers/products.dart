e79935070e66c41b04d5c703b2a5d194987ad590
André <andre2ar@outlook.com>
2020-05-20 15:01:45 -0300

    Filter

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -26,9 +26,11 @@ class Products with ChangeNotifier {
     return _items.firstWhere((product) => product.id == id);
   }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : null;
     var url =
-        'https://flutterstore-42eae.firebaseio.com/products.json?auth=$authToken';
+        'https://flutterstore-42eae.firebaseio.com/products.json?auth=$authToken&$filterString';
 
     try {
       final response = await http.get(url);
@@ -75,6 +77,7 @@ class Products with ChangeNotifier {
             'description': product.description,
             'price': product.price,
             'imageUrl': product.imageUrl,
+            'creatorId': userId,
           },
         ),
       );
